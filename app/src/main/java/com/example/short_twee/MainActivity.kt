bf6bda521f6a365da13258ad01352081a680fddb
arkan481 <arkanharyo@gmail.com>
2021-07-19 17:24:31 +0700

    Splash Screen UI was added

--- app/src/main/java/com/example/short_twee/MainActivity.kt
+++ app/src/main/java/com/example/short_twee/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.short_twee
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
+
+    private val isLoggedIn = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // after 3 seconds redirect to another page
+        Handler(Looper.getMainLooper()).postDelayed({
+            if (isLoggedIn) {
+                startActivity(Intent(this, ChatActivity::class.java))
+            } else {
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
+            finish()
+        }, 3000)
     }
 }
